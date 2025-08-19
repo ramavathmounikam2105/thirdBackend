67aa370b387fceed2e8797ad9c80a027b8ed5748
ramavathmounikam2105 <ramavathmounika2105@gmail.com>
2025-08-19 10:20:13 +0000

    thirdBackend

---
+++ server/app.js
@@ -0,0 +1,25 @@
+console.log("hello world")
+
+// import jwt from "jsonwebtoken";
+
+// function encryptData()
+// {
+//     let message = "Global is a college"
+//     console.log(message);
+
+//     let encryptMessage = jwt.sign({message}, "12345",{ expiresIn: "2h",});
+//     console.log(encryptMessage);
+// }
+// encryptData();
+
+// DecryptData
+
+// function decryptData()
+// {
+// let encryptedText=
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXNzYWdlIjoiR2xvYmFsIGlzIGEgY29sbGVnZSIsImlhdCI6MTc1NTUwNjYxMywiZXhwIjoxNzU1NTEwMjEzfQ.8Yhaaweo3DQgIYZ-HTrzXrCkmMt6KJlomDuDyPY6nHs"
+// let decryptedText = jwt.verify(encryptedText, "12345");
+// console.log(decryptedText);
+// }
+// decryptData();
+    
